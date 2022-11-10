4b131caaf2d33ce9f3e3d9daae878e7903dfcab4
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-10 14:57:28 +0400

    ml-4">{/*margin left

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import styles from '../styles/Global';
 import assets from '../assets';
+import Button from '../components/Button'
 
-const SectionWrapper = ({title, description,showBtn,mockupImg}) => {
+
+const SectionWrapper = ({title, description,showBtn,mockupImg, banner}) => {
   return (
-    <div className={`min-h-screen ${styles.section}`}>{/*чтобы передать реальные переменные */}
-    <h1>{title}</h1>
-    <h1>{description}</h1>
-    <img src={mockupImg} alt="mockup"/>
+    <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>{/*чтобы передать реальные переменные */}
+    <div className={`flex item-center ${styles.boxClass}`}>
+    <div className={`${styles.descDiv}
+    fadeLeftMini`}>
+    <h1 className={`${styles.h1Text}`}>
+    {title}</h1>
+    <p className={`${styles.descriptionText}`}>{description}</p>
+    {showBtn &&(
+        <Button
+        assetUrl={assets.expo}
+        link="deployed nft marketplace rn"
+        />
+    )}
+    </div>
+    </div>
+    
+    
+    
+    
+   {/*<img src={mockupImg} alt="mockup"/> */} 
     </div>
   )
 }
 
 export default SectionWrapper
+
+
+//banner появилась плавающая анимация волны и голубоватый фон
