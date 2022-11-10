bf67999f0ddd71cb41b282c2a6cf3f7d4f110bfa
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-11-10 13:37:45 +0400

    mockupImg={assets.homeHero} //макет изображения

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import {Download, Features, SectionWrapper} from './components';
+import assets from './assets';
+
+
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">React Native NFT Showcase</h1>
-    </div>
+    <>
+    <SectionWrapper
+    title="You own store of Nifty NFTs. Start Selling & Growing"
+    description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
+    showBtn
+    mockupImg={assets.homeHero} //макет изображения
+    banner="banner"
+    />
+    </>
   );
 };
 
